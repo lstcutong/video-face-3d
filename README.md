5c9a10e4536f277128aad967eaded3ee10c2c532
lstcutong <527083553@qq.com>
2020-06-09 14:49:16 +0800

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # video-face-3d
-人脸工具包大杂烩，包括视频人脸追踪，68关键点检测，三维人脸3DMM参数标注等
+人脸工具包大杂烩，主要整合了近年来的SOTA的一些方法，包括视频人脸追踪，68关键点检测，三维人脸3DMM参数标注等
+
+## 依赖
+
+- pytorch  0.4.1以上
+- tensorflow 1.0 以上
+- neural_renderer_pytorch
+
+
+
+## 安装
+
+安装本代码前请先确保安装了pytorch,tensorflow以及neural_renderer_pytorch
+
+- neural_renderer_pytorch安装说明
+
+  - 如果你是Linux或者Mac用户，请直接`pip install neural_renderer_pytorch`
+
+  - 如果你是Windows10用户，请参考[neural_renderer-win10](https://github.com/lstcutong/neural_renderer_pytorch-win10) 进行安装
+
+下载本代码依赖的静态资源文件，解压到`/VideoFace3D/data`，下载链接[data.zip](https://pan.baidu.com/s/1B4hSHIbg2fsXk6e0zN2jsg)，密码 gdfz
+
+推荐使用`develop`模式安装代码  `python setup.py develop`
+
+
+
+## TODO
+
+- example例程
+- api说明
+- SFS
---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import find_packages, setup
+
+
+files =["data/*"]
+
+setup(
+    description='utils for video face data preprocess, include video face tracking, landmark detecting, morphable face model fitting',
+    author='Luo Shoutong',
+    author_email='MF1933071@smail.nju.edu.cn',
+    license='Magic',
+    version='0.0.1',
+    name='VideoFace3D',
+    include_package_data = True,
+    packages=find_packages(),
+)
+
+'''
+"./data/BFM_front_idx.mat",
+"./data/BFM_model_front.mat",
+"./data/FaceReconModel.pb",
+"./data/phase1_wpdc_vdc.pth.tar",
+"./data/shape_predictor_68_face_landmarks.dat",
+"./data/similarity_Lm3D_all.mat",
+"./data/tri.mat",
+"./data/keypoints_sim.npy",
+"./data/Model_PAF.pkl",
+"./data/param_whitening.pkl",
+"./data/pncc_code.npy",
+"./data/u_exp.npy",
+"./data/u_shp.npy",
+"./data/w_exp_sim.npy",
+"./data/w_shp_sim.npy"
+'''
